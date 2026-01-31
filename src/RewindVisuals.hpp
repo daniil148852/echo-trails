02801523eaaba7a930201ca91f0d4b09e3f16a43
daniil148852 <daniildan850@gmail.com>
2026-01-31 09:22:38 +0100

    Update RewindVisuals.hpp

--- src/RewindVisuals.hpp
+++ src/RewindVisuals.hpp
@@ -1,1 +1,41 @@
+#pragma once
 
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+class RewindVisuals {
+private:
+    RewindVisuals() = default;
+    
+public:
+    static RewindVisuals* get();
+    
+    // Визуальные элементы
+    CCLayerColor* m_tintLayer = nullptr;
+    CCNode* m_effectContainer = nullptr;
+    CCSprite* m_vhsLines = nullptr;
+    std::vector<CCSprite*> m_ghostTrail;
+    
+    // Состояние
+    bool m_active = false;
+    float m_effectTime = 0.0f;
+    PlayLayer* m_playLayer = nullptr;
+    
+    // Настройки эффектов
+    ccColor3B m_rewindTint = ccc3(100, 150, 255);
+    float m_chromaticAberration = 3.0f;
+    float m_vhsIntensity = 0.5f;
+    
+    void startRewindEffect(PlayLayer* playLayer);
+    void updateEffect(float progress);
+    void stopRewindEffect();
+    void cleanup();
+    
+private:
+    void createTintOverlay();
+    void createVHSEffect();
+    void createGhostTrail();
+    void updateGhostTrail(float progress);
+    void updateVHSEffect(float progress);
+};
