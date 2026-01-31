1d75552b25cf2acb1d0dd8afffaba39315687d42
daniil148852 <daniildan850@gmail.com>
2026-01-31 09:34:58 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,8 @@
 #include <Geode/Geode.hpp>
-#include <Geode/loader/SettingEvent.hpp>
 #include "TimeRewindManager.hpp"
 
 using namespace geode::prelude;
 
 $on_mod(Loaded) {
     log::info("Time Rewind mod loaded!");
-    
-    // Подписываемся на изменение настроек
-    listenForSettingChanges("enabled", [](bool value) {
-        log::info("Time Rewind enabled: {}", value);
-    });
-    
-    listenForSettingChanges("rewind-duration", [](double value) {
-        TimeRewindManager::get()->m_rewindDuration = static_cast<float>(value);
-        TimeRewindManager::get()->loadSettings(); // Пересчитываем буфер
-    });
 }
